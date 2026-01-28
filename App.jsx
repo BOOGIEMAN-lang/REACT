b93ea1b16058a76dd7433b68414b118cd09945b5
BOOGIEMAN-lang <albionsejdiu08@icloud.com>
2026-01-28 13:47:11 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,43 +1,15 @@
-import React from 'react';
+import React from "react";
+import Infolista from "./Infolista";
 
+const App = () => {
+  const tiedot = ["React", "JavaScript", "CSS"];
 
-function Tervehdys({ nimi }) {
   return (
     <div>
-      <h1>Hei, {nimi}!</h1>
+      <h1>Teknologiat</h1>
+      <Infolista taulukko={tiedot} />
     </div>
   );
-}
-
-
-function OpiskelijaTiedot({ opiskelija }) {
-  return (
-    <div>
-      <h2>Opiskelijan tiedot</h2>
-      <p>Nimi: {opiskelija.nimi}</p>
-      <p>Ikä: {opiskelija.ika}</p>
-      <p>Kurssi: {opiskelija.kurssi}</p>
-    </div>
-  );
-}
-
-
-function App() {
-  const opiskelija = {
-    nimi: "Albion",
-    ika: 17,
-    kurssi: "Reactin perusteet"
-  };
-
-  return (
-    <div className="App">
-    
-      <Tervehdys nimi={opiskelija.nimi} />
-
-      
-      <OpiskelijaTiedot opiskelija={opiskelija} />
-    </div>
-  );
-}
+};
 
 export default App;
