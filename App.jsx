c357ed452d978b4679504e0c4380e5154dd6d69e
BOOGIEMAN-lang <albionsejdiu08@icloud.com>
2026-02-05 12:51:35 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import Infolista from "./Infolista";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const tiedot = ["React", "JavaScript", "CSS"];
+
+function Counter() {
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>Teknologiat</h1>
-      <Infolista taulukko={tiedot} />
+      <h2>Counter</h2>
+      <p>Arvo: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Kasvatus
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Vähennys
+      </button>
+
+      <button onClick={() => setCount(0)}>
+        Nollaus
+      </button>
     </div>
   );
-};
+}
 
-export default App;
+export default Counter;
